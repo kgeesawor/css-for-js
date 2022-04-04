aa271e08445ceb6886568a426f5514805d72b024
Kushal Geesawor <kgeesawor@coursera.org>
2022-04-04 06:15:30 -0400

    back to it!

--- mini-component/mini-component-library-main/src/components/ProgressBar/ProgressBar.js
+++ mini-component/mini-component-library-main/src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,65 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+
+  small: {
+    "--barHeight": 8 + "px",
+    "--textTop": 10 + "px",
+    "--padding": 2 + "px"
+  },
+  medium: {
+    "--barHeight": 12 + "px",
+    "--textTop": 14 + "px",
+    "--padding": 2 + "px"
+  },
+  large: {
+    "--barHeight": 24 + "px",
+    "--textTop": 26 + "px",
+    "--padding": 2 + "px"
+  }
+
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const leftEdge = value >= 98 ? 4 : 0;
+  const styles = SIZES[size];
+  return  <OuterBox style={styles}>
+            <InnerBox value={value} leftEdge={leftEdge}>
+            <VisuallyHidden>
+             <ProgressValue> 
+              {value}%
+             </ProgressValue>
+             </VisuallyHidden>
+            </InnerBox>
+          </OuterBox>;
 };
+
+
+const OuterBox = styled.div`
+  width: 370px;
+  height: var(--barHeight);
+  position:absolute;
+  background-color: ${COLORS.transparentGray35};
+  border-radius: 4px;
+`
+
+const InnerBox = styled.div`
+  width: ${p => p.value/100 * 366}px;
+  height: var(--barHeight);
+  position: relative;
+  background-color:  ${COLORS.primary};
+  left:2px;
+  border-radius: 4px ${p=>p.leftEdge}px ${p=>p.leftEdge}px 4px;
+`
+ 
+const ProgressValue = styled.div`
+position: relative;
+font-size: 18px;
+font-weight: bold; 
+top: var(--textTop);
+left: 168px;
+color: black;
+`
 
 export default ProgressBar;
