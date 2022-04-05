ee4a8729eec8ad0e4ec85f8d9842538750309dcd
Kushal Geesawor <kgeesawor@coursera.org>
2022-04-05 08:04:39 -0400

    finished part 2

--- mini-component/mini-component-library-main/src/components/Select/Select.js
+++ mini-component/mini-component-library-main/src/components/Select/Select.js
@@ -9,10 +9,64 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <Wrapper>
+      <SelectDiv value={value} onChange={onChange}>
       {children}
-    </select>
+      </SelectDiv>
+      <BaseDiv>
+       {displayedValue}
+      <IconWrapper style = {{'--size': 24 + 'px'}}>
+        <Icon id = "chevron-down" strokeWidth={1} size={24}/>
+      </IconWrapper>
+      </BaseDiv>
+      </Wrapper>
   );
 };
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+`
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+  isolation: isolate;
+`
+
+const SelectDiv = styled.select`
+  position:absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  z-index: 2;
+`
+
+const BaseDiv = styled.div`
+  z-index:1;
+  background-color: red;
+  padding: 12px 16px;
+  padding-right: 52px;
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  font-size: ${16/16} rem;
+  border-radius: 4px;
+
+  ${SelectDiv}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+
+  ${SelectDiv}:hover + & {
+    color: black;
+  }
+`
 
 export default Select;
