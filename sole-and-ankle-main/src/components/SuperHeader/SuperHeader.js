d09b24636aaed5aabe5835d3365f7b82ea6d0604
Kushal Geesawor <kgeesawor@coursera.org>
2022-04-24 14:56:03 -0400

    ex. 1 superheader

--- sole-and-ankle-main/src/components/SuperHeader/SuperHeader.js
+++ sole-and-ankle-main/src/components/SuperHeader/SuperHeader.js
@@ -13,8 +13,10 @@ const SuperHeader = () => {
       <MarketingMessage>
         Free shipping on domestic orders over $75!
       </MarketingMessage>
+      <SearchWrapper>
       <SearchInput />
       <HelpLink href="/help">Help</HelpLink>
+      </SearchWrapper>
       <UnstyledButton>
         <Icon id="shopping-bag" strokeWidth={1} />
       </UnstyledButton>
@@ -23,19 +25,35 @@ const SuperHeader = () => {
 };
 
 const Wrapper = styled.div`
-  font-size: 0.875rem;
+  font-size: 14px;
+  font-weight: 500px;
+  padding: 8px 32px;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  justify-content: space-evenly;
 `;
+
+const SearchWrapper = styled.div`
+  margin-left: auto;
+  display: flex;
+  justify-content: space-between;
+`
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  display: flex;
+  align-items: center;
 `;
 
 const HelpLink = styled.a`
   color: inherit;
   text-decoration: none;
   outline-offset: 2px;
+  padding: 0 24px;
+  display: flex;
+  align-items: center;
+
 
   &:not(:focus-visible) {
     outline: none;
